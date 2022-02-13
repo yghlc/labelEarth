3773077d7fe57c19240e24f7b2b1c1d1722d80fd
guiyli <guiyli@ttu.edu>
2022-02-13 11:01:03 -0700

    update small demos

--- js/index.js
+++ js/index.js
@@ -1,15 +1,23 @@
-      // Initialize and add the map
-      function initMap() {
-        // The location of Uluru
-        const uluru = { lat: -25.344, lng: 131.036 };
-        // The map, centered at Uluru
-        const map = new google.maps.Map(document.getElementById("map"), {
-          zoom: 4,
-          center: uluru,
-        });
-        // The marker, positioned at Uluru
-        const marker = new google.maps.Marker({
-          position: uluru,
-          map: map,
-        });
-      }
+// initialize Leaflet
+      var map = L.map('div3').setView({
+      	lon: 0,
+      	lat: 0
+      }, 2);
+
+      // add the OpenStreetMap tiles
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      	maxZoom: 19,
+      	attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+      }).addTo(map);
+
+      // // show the scale bar on the lower left corner
+      // L.control.scale({
+      // 	imperial: true,
+      // 	metric: true
+      // }).addTo(map);
+
+      // // show a marker on the map
+      // L.marker({
+      // 	lon: 0,
+      // 	lat: 0
+      // }).bindPopup('The center of the world').addTo(map);
