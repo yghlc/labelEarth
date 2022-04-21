68ebaff622ef5d9791ec116a9c20a8ffd2a1c19b
Lingcao Huang <huanglingcao@gmail.com>
2022-04-21 16:59:40 -0600

    get image info and show them

--- infoForm/infoFormEdit.js
+++ infoForm/infoFormEdit.js
@@ -2,21 +2,34 @@
 
 let image_list;  //
 const serUrl = 'http://127.0.0.1:8000/imageObjects';
-
-// load the data and shows the first images
-InitialForm_FirstItem();
+const Http = new XMLHttpRequest()
 
 
-function InitialForm_FirstItem(){
-    // need user login, so we can get the username
-    let username = 'huanglingcao'
-    let getitem_url = serUrl + '/' + username + '/' + 'getitem';
-
-    fetch(getitem_url).
-    then(response=>response.json()).
-    then(data => console.log(data))
+async function get_available_image(username){
+    // wait and get image info: {image name, center lat, lon}
+    let item_url = serUrl + '/' + username + '/' + 'getitem';
+    let response = await fetch(item_url);
+    if (response.ok){
+        let data = await response.json();
+        return data;
+    }
 }
 
+async function InitialForm_FirstItem(){
+    // need user to login first, so we can get the username
+    let username = 'huanglingcao'
+    let img_info = await get_available_image(username);
+    // console.log(img_info)
+    return img_info
+}
+
+// load the data and shows the first images after login
+InitialForm_FirstItem().then(img_info => {
+        console.log(img_info);
+        let image_name = document.getElementById('image_name');
+        image_name.value  = `Name: ${img_info.image_name}, Center Lat: ${img_info.image_center_lat}, Center Lon: ${img_info.image_center_lon}`;
+    }
+)
 
 function submitAndNext(){
     alert("submitAndNext in infoFormEdit");
